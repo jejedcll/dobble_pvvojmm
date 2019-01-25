6a8e555ade1bb37f7526844e67eaffffe12c9545
etudiant <jerome.martinsmosca@gmail.com>
2019-01-25 15:14:29 +0100

    ajout du suivi du gitignore

---
+++ src/main/java/frunivangers/jpv/Frame.java
@@ -0,0 +1,33 @@
+package main.java.frunivangers.jpv;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import javax.swing.DropMode;
+import javax.swing.JButton;
+
+public class Frame extends JFrame{
+	
+	public Frame(String title) {
+		setTitle(title);
+		setSize(1200, 700);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+        JTextField message = new JTextField(30);
+        JButton envoyer = new JButton("envoyer");
+		JTextArea chat = new JTextArea();
+		chat.setEditable(false);
+		chat.setEditable(false);
+		JPanel contentpane = (JPanel)getContentPane();
+		setVisible(true);
+		contentpane.setLayout(null);
+		contentpane.add(chat,BorderLayout.CENTER);
+		
+		
+	}
+	
+}
